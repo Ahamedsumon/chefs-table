3ca1f600a02006cb2329edd7ef0a579f4625d500
Ahamed Sumon <ahamedsumon13579@gmail.com>
2026-01-25 16:47:59 +0300

    addeded header section

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./components/header/Header";
+import Landingpage from "./components/landing-page/Landingpage";
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Landingpage></Landingpage>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/landing-page/Landingpage.jsx
@@ -0,0 +1,9 @@
+const Landingpage = () => {
+  return (
+    <div className="max-w-7xl mx-auto">
+      <h2>landing page</h2>
+    </div>
+  );
+};
+
+export default Landingpage;
